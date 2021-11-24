f0fad09f0dd852afe6ba1d1dbc311cc24498919b
mich3121 <michalch99@gmail.com>
2021-11-24 18:12:24 +0100

    dodanie slef

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ class Nowa:
    faces = face_Cascade.detectMultiScale(imgGray, 1.1, 4)
    for (x, y, w, h) in faces:
     self.image[x:x + w, y:y + h] = cv2.resize(self.image2, (w, h))
-    cv2.imshow(self.image)
+
+   cv2.imshow(self.image)
 
 
   window = Tk()
@@ -33,8 +34,8 @@ class Nowa:
   label2 = Label(window, text = "Wybierz 2:").place(x = 5, y = 25)
   label3 = Label(window, text = "(test)Zapisz jako:").place(x = 5, y = 55) # testy
   b1 = Button(window, text = "Pierwsze zdjecie", command = file_path(self=None)).pack()
-  b2 = Button(window, text = "Drugie zdjecie", command = file_path2(self=None).pack()
-  b3 = Button(window, text = "Pokaz efekt", command = faceswap).pack()
+  b2 = Button(window, text = "Drugie zdjecie", command = file_path2(self=None)).pack()
+  b3 = Button(window, text = "Pokaz efekt", command = faceswap(self=None)).pack()
 
   window.mainloop()
   cv2.waitKey(0)
