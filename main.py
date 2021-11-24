db8c087bb193360070a1d77ec2ba282094aa6f49
mich3121 <michalch99@gmail.com>
2021-11-24 12:55:47 +0100

    nadal nie dziala

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from tkinter import filedialog
 
 class Nowa:
 
+ def filepath(image):
   file_path = filedialog.askopenfilename(title="Wybierz plik", filetypes=(("jpg", "*.jpg"), ("png", "*.png")))
-#  l1 = Label(window, text=file_path).pack()
+  l1 = Label(window, text=file_path).pack()
 
-
+ def filepath2(image2):
   file_path2 = filedialog.askopenfilename(title="Wybierz plik", filetypes=(("jpg", "*.jpg") ,("png", "*.png") ))
-#  l2 = Label(window, text=file_path2).pack()
+  l2 = Label(window, text=file_path2).pack()
 
+ def faceswap(file_path, file_path2,image,  image2):
   image = cv2.imread(file_path)
   image2 = cv2.imread(file_path2)
   imgGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -21,21 +23,20 @@ class Nowa:
   faces = face_Cascade.detectMultiScale(imgGray, 1.1, 4)
   for (x, y, w, h) in faces:
    image[x:x + w, y:y + h] = cv2.resize(image2, (w, h))
+  cv2.imshow(image)
 
-  pokaz=cv2.imshow(image)
+ window = Tk()
+ window.title("Po wybraniu zamknij okno...")
+ window.geometry("400x130")
+ label1 = Label(window, text = "Wybierz 1:").place(x = 5, y = 0)
+ label2 = Label(window, text = "Wybierz 2:").place(x = 5, y = 25)
+ label3 = Label(window, text = "(test)Zapisz jako:").place(x = 5, y = 55) # testy
+ b1 = Button(window, text = "Pierwsze zdjecie", command = filepath).pack()
+ b2 = Button(window, text = "Drugie zdjecie", command = filepath2).pack()
+ b3 = Button(window, text = "Pokaz efekt", command = faceswap).pack()
 
-  window = Tk()
-  window.title("Po wybraniu zamknij okno...")
-  window.geometry("400x130")
-  label1 = Label(window, text = "Wybierz 1:").place(x = 5, y = 0)
-  label2 = Label(window, text = "Wybierz 2:").place(x = 5, y = 25)
-  label3 = Label(window, text = "(test)Zapisz jako:").place(x = 5, y = 55) # testy
-  b1 = Button(window, text = "Pierwsze zdjecie", command = file_path).pack()
-  b2 = Button(window, text = "Drugie zdjecie", command = file_path2).pack()
-  b3 = Button(window, text = "Pokaz efekt", command = cv2.imshow(image)).pack()
-
-  window.mainloop()
+ window.mainloop()
 
 
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()
+ cv2.waitKey(0)
+ cv2.destroyAllWindows()
