d48d70bc1c255fd83568103bd675c76d6ff91bf3
mich3121 <michalch99@gmail.com>
2021-11-25 09:03:07 +0100

    dalesze  proby

--- main.py
+++ main.py
@@ -18,10 +18,9 @@ class App(object):
    self.image2 = cv2.imread(self.file_path2)
   def file_path(self):
    self.file_path = filedialog.askopenfilename(title="Wybierz plik", filetypes=(("jpg", "*.jpg"), ("png", "*.png")))
-   l1 = Label(window, text=self.file_path).pack()
   def file_path2(self):
    self.file_path2 = filedialog.askopenfilename(title="Wybierz plik", filetypes=(("jpg", "*.jpg") ,("png", "*.png") ))
-   l2 = Label(window, text=self.file_path2).pack()
+
 
   def faceswap(self):
    imgGray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
@@ -40,8 +39,8 @@ class AppRun(App):
   label1 = Label(window, text = "Wybierz 1:").place(x = 5, y = 0)
   label2 = Label(window, text = "Wybierz 2:").place(x = 5, y = 25)
   label3 = Label(window, text = "(test)Zapisz jako:").place(x = 5, y = 55) # testy
-  b1 = Button(window, text = "Pierwsze zdjecie", command = App.file_path(self=file_path)).pack()
-  b2 = Button(window, text = "Drugie zdjecie", command = App.file_path2(file_path2)).pack()
+  b1 = Button(window, text = "Pierwsze zdjecie", command = App.file_path(self=None)).pack()
+  b2 = Button(window, text = "Drugie zdjecie", command = App.file_path2(self=None)).pack()
   b3 = Button(window, text = "Pokaz efekt", command = App.faceswap(self=None)).pack()
 
   window.mainloop()
