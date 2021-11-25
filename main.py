ad0b0f87f7fb09ee29f45c1ef0dcafc862b90762
mich3121 <michalch99@gmail.com>
2021-11-25 09:21:24 +0100

    commit

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from math import hypot
 from tkinter import *
 from tkinter import filedialog
 
+global file_path
+global file_path2
+
 
 def get_file_path():
-  global file_path
   file_path = filedialog.askopenfilename(title="Wybierz plik", filetypes=(("jpg", "*.jpg"), ("png", "*.png")))
   l1 = Label(window, text=file_path).pack()
 
 
 def get_file_path2():
-  global file_path2
   file_path2 = filedialog.askopenfilename(title="Wybierz plik", filetypes=(("jpg", "*.jpg") ,("png", "*.png") ))
   l2 = Label(window, text=file_path2).pack()
 
@@ -35,7 +36,7 @@ label2 = Label(window, text = "Wybierz 2:").place(x = 5, y = 25)
 label3 = Label(window, text = "(test):").place(x = 5, y = 55) # testy
 b1 = Button(window, text = "Pierwsze zdjecie", command = get_file_path).pack()
 b2 = Button(window, text = "Drugie zdjecie", command = get_file_path2).pack()
-b3 = Button(window, text = "Pokaz efekt", command = swapface).pack()
+b3 = Button(window, text = "Pokaz efekt", command = swapface(file_path, file_path2)).pack()
 
 window.mainloop()
 
