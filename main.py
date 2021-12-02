ce3c1d6471b61a6a98c6686613fbff7ac02c6b49
Chojar-alt <ml.chojaczyk+1@gmail.com>
2021-12-02 19:39:57 +0100

    Skończone zadanie, bez kasowania tła, bez tensorflow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import cv2
-import numpy as np
-#import dlib
-from math import hypot
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image
@@ -14,7 +11,6 @@ image = ""
 def get_file_path():
  global file_path
  file_path = filedialog.askopenfilename(title="Wybierz plik", filetypes=(("jpg", "*.jpg"), ("png", "*.png")))
- #print("sciezka : {}".format(file_path))
  l1 = Label(window, text=file_path).pack()
 
 
@@ -27,19 +23,14 @@ def swapface():
  global file_path
  global file_path2
  global image
- #print("sciezka : {}".format(file_path))
  image = cv2.imread(file_path)
  image2 = cv2.imread(file_path2)
- #print (image)
  imgGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
  face_Cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
  faces = face_Cascade.detectMultiScale(imgGray, 1.1, 4)
  for (x, y, w, h) in faces:
   image[x:x + w, y:y + h] = cv2.resize(image2, (w, h))
   return image
-  #cv2.imshow('img', image)
-  #cv2.imwrite('result.jpg', image)
- #window.destroy()
 
 def showface():
  global image
@@ -53,21 +44,20 @@ def saveas():
  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
  im_pil = Image.fromarray(image)
  a = filedialog.asksaveasfile(defaultextension=".jpg")
- print("sciezka : {}".format(a))
  im_pil.save(a)
 
 
 
 window = Tk()
 window.title("Po wybraniu zamknij okno...")
-window.geometry("450x300")
+window.geometry("450x200")
 label1 = Label(window, text = "Wybierz 1:").place(x = 5, y = 0)
 label2 = Label(window, text = "Wybierz 2:").place(x = 5, y = 25)
-label3 = Label(window, text = "Sprawdź ").place(x = 5, y = 55) # testy
+label3 = Label(window, text = "Sprawdź ").place(x = 5, y = 55)
 b1 = Button(window, text = "Pierwsze zdjecie", command = get_file_path).pack()
 b2 = Button(window, text = "Drugie zdjecie", command = get_file_path2).pack()
-b3 = Button(window, text = "Pokaz efekt i zapisz", command = showface).pack()
-b4 = Button(window, text = "save  as", command = saveas).pack()
+b3 = Button(window, text = "Pokaz efekt", command = showface).pack()
+b4 = Button(window, text = "Zapisz jako", command = saveas).pack()
 
 window.mainloop()
 
